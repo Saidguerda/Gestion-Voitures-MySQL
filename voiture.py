b1be66627300aabbc654e0e832159db0f0c89637
LE BON COIN <saidguerda04@gmail.com>
2026-04-09 15:44:20 -0400

    je viens de créer la class Voiture avec ces attribut

---
+++ voiture.py
@@ -0,0 +1,7 @@
+class Voiture:
+    def __init__(self, marque, modele, annee, prix, id=None):
+        self.id = id
+        self.marque = marque
+        self.modele = modele
+        self.annee = annee
+        self.prix = prix
