bdddf5e3b2b7fd33a5deecfbf8f427485dd74666
LE BON COIN <saidguerda04@gmail.com>
2026-04-10 11:10:32 -0400

    j'ai créer 3 objects de la classe voiture et je l'ai insérer dans la table

--- main.py
+++ main.py
@@ -4,3 +4,13 @@ conn = connecter_db()
 print("Connexion réussie !")
 conn.close()
 
+from voiture import Voiture
+from crud_db import ajouter_voiture
+
+v1 = Voiture("bmw", "M3", 2020, 20000)
+v2 = Voiture("Honda", "Civic", 2019, 18000)
+v3 = Voiture("hyundai", "Elantra", 2019, 18000)
+
+ajouter_voiture(v1)
+ajouter_voiture(v2)
+ajouter_voiture(v3)
